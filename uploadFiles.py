4d39dd4f499ed9f8e9fd950732633251f57bfa4c
pgrstephen1 <74113003+pgrstephen1@users.noreply.github.com>
2022-07-09 13:09:36 -0400

    Add files via upload

---
+++ uploadFiles.py
@@ -0,0 +1,27 @@
+from winreg import DeleteKey
+import dropbox
+import os
+
+class TransferData:
+    def __init__(self, access_token):
+        self.access_token = access_token
+
+    def upload_file(self, folder_from, folder_to):
+         dbx = dropbox.Dropbox(self.access_token)
+         for root, dirs, files in os.walk(file_from):
+            relative_path = os.path.relpath(local_path, file_from)
+            dropbox_path = os.path.join(file_to, relative path)
+        with open(local_path, 'rb') as f:
+            dbx.files_upload(f.read(), dropbox_path, mode=WriteMode('overwrite'))
+        
+def main():
+    access_token = ''
+    transferData = TransferData(access_token)
+
+    file_from = input("Enter the file path to transfer : -")
+    file_to = input("enter the full path to upload to dropbox:- ")  # This is the full path to upload the file to, including name that you wish the file to be called once uploaded.
+
+    # API v2
+    transferData.upload_file(file_from, file_to)
+
+main()
